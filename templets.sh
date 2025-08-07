898e97d18cb74cc41314f6736771c59327996a10
sanjith chilupuri <141348079+sanjithrana@users.noreply.github.com>
2025-08-07 16:29:54 +0530

    floders added

---
+++ templets.sh
@@ -0,0 +1,18 @@
+#creating the directorys
+mkdir -p src
+mkdir -p researchs
+
+#creating the files using in one floder .sh
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+
+touch .env
+touch setup.py
+touch app.py
+touch requriments.txt
+
+touch researchs/trails.ipynb
+
+echo "Directory and  files created successfully!."
