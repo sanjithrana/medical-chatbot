c17d9fe3a4a80475c87da888acc0834ee12f8496
sanjith chilupuri <141348079+sanjithrana@users.noreply.github.com>
2025-08-07 17:36:27 +0530

    Requirments added

--- templets.sh
+++ templets.sh
@@ -1,3 +1,4 @@
+#create a templets.sh file
 #creating the directorys
 mkdir -p src
 mkdir -p researchs
@@ -16,3 +17,13 @@ touch requriments.txt
 touch researchs/trails.ipynb
 
 echo "Directory and  files created successfully!."
+
+#in git bash type sh templets.sh
+#add this all floders into git add . , git commit -m " " , git push origin main
+
+#for env 
+#conda #create -n medical_chatbot python=3.10 -y
+#conda activate medical_chatbot
+#pip install -r requriments.txt
+
+#now go to setup files
